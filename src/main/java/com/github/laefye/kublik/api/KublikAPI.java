f3906457759429f727d71f192766c828b3ce13b5
Laefye <rtpurtooe22@mail.ru>
2023-05-13 16:04:16 +0300

    Initial commit

---
+++ src/main/java/com/github/laefye/kublik/api/KublikAPI.java
@@ -0,0 +1,7 @@
+package com.github.laefye.kublik.api;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public abstract class KublikAPI extends JavaPlugin {
+    public abstract Text getText();
+}
---
+++ src/main/java/com/github/laefye/kublik/api/Text.java
@@ -0,0 +1,5 @@
+package com.github.laefye.kublik.api;
+
+public abstract class Text {
+    public abstract String format(String text);
+}
